898e5982028a552b15ee4661af5301ce467e60e8
Marcos Fernandez <marcosezq@gmail.com>
2022-01-23 18:20:52 -0300

    Proyecto de mercado liebre

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"))
+
+let puerto=3003
+
+app.get("/home",(req,res)=>{
+    res.sendFile(__dirname + "/views/home.html")
+})
+
+app.get("/registro",(req,res)=>{
+    res.sendFile(__dirname + "/views/registro.html")
+})
+
+app.get("/ingresar",(req,res)=>{
+    res.sendFile(__dirname + "/views/ingresar.html")
+})
+
+
+app.listen(puerto,()=>{
+    console.log ("Servidor corriendo en el puerto",puerto)
+})
+
